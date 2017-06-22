e270c6d5987176c07df4b6679c5bf686423f1e81
吴佩 <877307291@qq.com>
2017-06-22 11:28:46 +0800

    Initial Commit

---
+++ RuntimeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeDemo
+//
+//  Created by wupei on 2017/6/22.
+//  Copyright © 2017年 WP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RuntimeDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RuntimeDemo
+//
+//  Created by wupei on 2017/6/22.
+//  Copyright © 2017年 WP. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
